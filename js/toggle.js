ff8eada9cb574a5c316401f29ce66c4f72a3a47c
Omer Evron <omerevron011@gmail.com>
2022-09-27 14:42:58 +0300

    Done

--- js/toggle.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('body');
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
